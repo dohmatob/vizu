3baa437b97c8adb2a65cdb8221b67ad1d91c13e2
Elvis DOHMATOB <elvis.dohmatob@inria.fr>
2015-02-14 11:21:54 +0100

    cosmetics + doc

--- make_gif.sh
+++ make_gif.sh
@@ -6,6 +6,8 @@
 # (c) DOHMATOB [Elvis] Dopgima <gmdopp@gmail.com
 ###################################################
 
+: {FSL_VERS=5.0}
+
 function usage {
     printf "\tUsage: bash $0 <input_4D_image> [scaling] [xslice] [yslice] [zslice]\n\n"
     exit 1
@@ -16,22 +18,27 @@ if [ "$1" = "" ]; then
     usage
 fi
 
+# configure FSL
+if [ ! -e /etc/fsl/${FSL_VERS}/fsl.sh ]; then
+    echo "ERROR: FSL config file /etc/fsl/${FSL_VERS}/fsl.sh doesn't exist!"
+    echo "Set FSL_VERS (current value if ${FSL_VERS} according to your FSL version, and rerun."
+    exit 1
+fi
+source /etc/fsl/${FSL_VERS}/fsl.sh  # Change the 5.0 to your FSL version
+
 # full verbose: report everythx happenx
 set -ex
 
-# configure FSL
-source /etc/fsl/5.0/fsl.sh  # Change the 5.0 to your FSL version
-
 # get input
-input_4D_image=`remove_ext $1`
+input_4D_img=`remove_ext $1`
 scaling=${2:-4}
 xslice=${3:-0.5}
 yslice=${4:-0.5}
 zslice=${5:-0.5}
 
 # split input 4d image into seperate 3d images, and walk them
-fslsplit ${input_4D_image} ${input_4D_image}_DEADBEEF -t
-for vol in `imglob ${input_4D_image}_DEADBEEF????.nii.gz`; do
+fslsplit ${input_4D_img} ${input_4D_img}_DEADBEEF -t
+for vol in `imglob ${input_4D_img}_DEADBEEF????.nii.gz`; do
     # generate a plot per dimension
     slicer ${vol} -s ${scaling} -x ${xslice} ${vol}_x.png
     slicer ${vol} -s ${scaling} -y ${yslice} ${vol}_y.png
@@ -42,8 +49,8 @@ for vol in `imglob ${input_4D_image}_DEADBEEF????.nii.gz`; do
 done
 
 # merge all per-volume thumbnails into an animated gif mivie
-convert -delay 10 ${input_4D_image}_DEADBEEF????_xyz.png ${input_4D_image}_movie.gif
-echo "Done. Output movie written to ${input_4D_image}_movie.gif"
+convert -delay 10 ${input_4D_img}_DEADBEEF????_xyz.png ${input_4D_img}_movie.gif
+echo "Done. Output movie written to ${input_4D_img}_movie.gif"
 
 # cleanup
-rm -f ${input_4D_image}_DEADBEEF????.nii.gz
+rm -f ${input_4D_img}_DEADBEEF????.nii.gz
